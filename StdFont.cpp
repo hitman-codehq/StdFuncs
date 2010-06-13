5542c316a3832302d0dd142f169d799037539845
Colin Ward <colin.ward@codehq.org>
2010-06-13 09:33:23 +0000

    Implemented RFont::DrawCursor() for OS4

--- StdFont.cpp
+++ StdFont.cpp
@@ -67,11 +67,35 @@ void RFont::DrawCursor(const char *a_pcText, TInt a_iX, TInt a_iY)
 {
 	ASSERTM(m_poWindow, "RFont::DrawCursor() => Window handle not set");
 
+	char Cursor[1] = { '@' };
+
 #ifdef __amigaos4__
+
+	int Width;
+
+	(void) a_pcText;
+
+	/* Move to the position at which to print, taking into account the left and top border sizes, */
+	/* the height of the current font and the baseline of the font, given that IGraphics->Text() */
+	/* routine prints at the baseline position, not the top of the font */
+
+	// TODO: CAW - This should work with proportional fonts
+	Width = m_poWindow->m_poWindow->IFont->tf_XSize;
+
+	IGraphics->Move(m_poWindow->m_poWindow->RPort, (m_poWindow->m_poWindow->BorderLeft + (a_iX * Width)),
+		(m_poWindow->m_poWindow->BorderTop + (a_iY * m_iHeight) + m_iBaseline));
+
+	/* Set the background and foreground pens, in case they have been changed since the last call */
+
+	IGraphics->SetAPen(m_poWindow->m_poWindow->RPort, 1);
+	IGraphics->SetBPen(m_poWindow->m_poWindow->RPort, 0);
+
+	/* And draw the text passed in */
+
+	IGraphics->Text(m_poWindow->m_poWindow->RPort, Cursor, 1);
 
 #else /* ! __amigaos4__ */
 
-	char Cursor[1] = { '@' };
 	SIZE Size;
 
 	/* Given the X position of the cursor, determine the number of pixels between the left of */
