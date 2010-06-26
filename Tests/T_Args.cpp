b7e8b4f4d5895f6a82787873248cc7ff3c876433
Colin Ward <colin.ward@codehq.org>
2010-06-26 07:57:36 +0000

    Added some tests for handling white space and " separators to T_Args.cpp

--- Tests/T_Args.cpp
+++ Tests/T_Args.cpp
@@ -3,14 +3,20 @@
 #include <Args.h>
 #include "Test.h"
 
-/* Fake command line used for testing, with parameters shuffled up to exercise RArgs class */
+/* Fake command lines used for testing, with parameters shuffled up to exercise RArgs class */
 
-const char *a_pccArgV[] =
+const char *g_pccArgV[] =
 {
-	"T_Args", "SourceDir", "DestDir", "delete", "copy", "noerrors", "deletedirs", "noprotect", "nocase"
+	"T_Args", "SourceDir", "DestDir", "copy", "delete", "deletedirs", "nocase", "noerrors", "noprotect"
 };
 
 #define ARGV_COUNT 9
+
+/* Strings for testing white space and " handling */
+
+const char *g_pccOneString = "\"Source Dir\" \"Dest Dir\" copy";
+const char *g_pccOneStringExtraWhite = " \"Source Dir\" \t \"Dest Dir\"  copy \t ";
+const char *g_pccOneStringNoWhite = "\"Source Dir\"\"Dest Dir\"copy";
 
 /* Template for use in obtaining command line parameters */
 
@@ -29,44 +35,98 @@ static const char g_accTemplate[] = "SOURCE/A,DEST/A,COPY/S,DELETE/S,DELETEDIRS/
 #define ARGS_NOPROTECT 7
 #define ARGS_NUM_ARGS 8
 
+#define CHECK_ARG(a, b) Test.Printf("Checking \"%s\" against \"%s\"\n", a, b); test(strcmp(a, b) == 0);
+
 static RDir g_oDir;				/* RDir class is global to implicitly test re-use */
 static RTest Test("T_Args");	/* Class to use for testing and reporting results */
 
 int main()
 {
-	int Result;
+	char *OneString;
+	int Index, Result;
 	RArgs Args;
 
 	Test.Title();
 	Test.Start("RArgs class API test");
-	Test.Next("Reading directory");
 
 	/* Test that Close() can handle being called before Open() */
 
+	Test.Next("Check calling close before open");
 	Args.Close();
 
-	/* Parse the command line parameters passed in and make sure they are formatted correctly */
+	/* Parse simple command line parameters that contain no white space */
 
-	Result = Args.Open(g_accTemplate, ARGS_NUM_ARGS, a_pccArgV, ARGV_COUNT);
+	Test.Next("Parse simple command line arguments");
+	Result = Args.Open(g_accTemplate, ARGS_NUM_ARGS, g_pccArgV, ARGV_COUNT);
 	test(Result == KErrNone);
 	test(Args.Count() == ARGS_NUM_ARGS);
 
-	/* Print out the list of argument for manual checking */
+	/* Ensure that the entries have been read as expected */
 
-	for (int Index = 0; Index < ARGS_NUM_ARGS; ++Index)
+	for (Index = 0; Index < (ARGV_COUNT - 1); ++Index)
 	{
-		if (Args[Index])
-		{
-			Test.Printf("%s\n", Args[Index]);
-		}
-		else
-		{
-			Test.Printf("Argument %d not specified\n", Index);
-		}
+		Test.Printf("Checking \"%s\" against \"%s\"\n", Args[Index], g_pccArgV[Index + 1]);
+		test(strcmp(Args[Index], g_pccArgV[Index + 1]) == 0);
 	}
 
 	Args.Close();
 
+	/* Parse more complex command line parameters that contain white space */
+
+	Test.Next("Parse complex command line arguments");
+
+	OneString = new char[strlen(g_pccOneString) + 1];
+	test(OneString != NULL);
+	strcpy(OneString, g_pccOneString);
+
+	Result = Args.Open(g_accTemplate, ARGS_NUM_ARGS, OneString);
+	test(Result == KErrNone);
+	test(Args.Count() == ARGS_NUM_ARGS);
+
+	/* Ensure that the entries have been read as expected */
+
+	CHECK_ARG(Args[0], "Source Dir");
+	CHECK_ARG(Args[1], "Dest Dir");
+	CHECK_ARG(Args[2], "copy");
+
+	Args.Close();
+
+	/* Parse more complex command line parameters that contain extra white space */
+
+	Test.Next("Parse complex command line arguments with extra white space");
+
+	strcpy(OneString, g_pccOneStringExtraWhite);
+	Result = Args.Open(g_accTemplate, ARGS_NUM_ARGS, OneString);
+	test(Result == KErrNone);
+	test(Args.Count() == ARGS_NUM_ARGS);
+
+	/* Ensure that the entries have been read as expected */
+
+	CHECK_ARG(Args[0], "Source Dir");
+	CHECK_ARG(Args[1], "Dest Dir");
+	CHECK_ARG(Args[2], "copy");
+
+	Args.Close();
+
+	/* Parse more complex command line parameters that contain white space without space separaters */
+	/* between " argument terminators */
+
+	Test.Next("Parse complex command line arguments lacking white space");
+
+	strcpy(OneString, g_pccOneStringNoWhite);
+	Result = Args.Open(g_accTemplate, ARGS_NUM_ARGS, OneString);
+	test(Result == KErrNone);
+	test(Args.Count() == ARGS_NUM_ARGS);
+
+	/* Ensure that the entries have been read as expected */
+
+	CHECK_ARG(Args[0], "Source Dir");
+	CHECK_ARG(Args[1], "Dest Dir");
+	CHECK_ARG(Args[2], "copy");
+
+	Args.Close();
+
+	delete [] OneString;
 	Test.End();
 
 	return(RETURN_OK);
