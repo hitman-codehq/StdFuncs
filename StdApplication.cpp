d103a4507e823d792e608e09c14c6fe3e7df237f
Colin Ward <colin.ward@codehq.org>
2010-07-01 07:02:54 +0000

    Implemented generic menu handling for OS4

--- StdApplication.cpp
+++ StdApplication.cpp
@@ -1,20 +1,127 @@
 
 #include "StdFuncs.h"
+#include <proto/gadtools.h>
+#include <proto/intuition.h>
 #include "StdApplication.h"
 #include "StdWindow.h"
-#include <stdio.h>
 
 /* Written: Saturday 26-Jun-2010 11:53 am */
 
 RApplication::RApplication()
 {
 
-#ifdef WIN32
+#ifdef __amigaos__
+
+	m_bDone = m_bMenuStripSet = EFalse;
+	m_poMenus = NULL;
+
+#else /* ! __amigaos__ */
 
 	m_poAccelerators = NULL;
 
-#endif /* WIN32 */
+#endif /* ! __amigaos__ */
 
+	m_pcoMenuItems = NULL;
+}
+
+#ifdef __amigaos__
+
+/* Written: Wednesday 30-Jun-2010 6:53 am */
+/* @param a_pcoMenuItems Ptr to array of menu items to be created.  Must be persisted for the lifetime */
+/*                       of the RApplication class that uses them */
+
+TInt RApplication::CreateMenus(const struct SStdMenuItem *a_pcoMenuItems)
+{
+	TInt Index, NumMenuItems, RetVal;
+	const struct SStdMenuItem *MenuItem;
+	struct NewMenu *NewMenus;
+	struct Screen *Screen;
+	APTR VisualInfo;
+
+	/* Assume failure */
+
+	RetVal = KErrNoMemory; // TODO:CAW - Proper error + what about Utils::Info() handling?
+
+	/* Iterate through the menu item structures passed in and count how many need to be created */
+
+	NumMenuItems = 1;
+	MenuItem = a_pcoMenuItems;
+
+	do
+	{
+		++NumMenuItems;
+		++MenuItem;
+	}
+	while (MenuItem->m_eType != EStdMenuEnd);
+
+	/* Save the ptr to the original menu item structures as it can be used for menu item */
+	/* command translation later */
+
+	m_pcoMenuItems = a_pcoMenuItems;
+
+	/* Allocate a buffer large enough to hold all of the GadTools NewMenu structures and populate */
+	/* them with data taken from the generic SStdMenuItem structures passed in */
+
+	if ((NewMenus = new NewMenu[NumMenuItems]) != NULL)
+	{
+		for (Index = 0; Index < NumMenuItems; ++Index)
+		{
+			NewMenus[Index].nm_Type = a_pcoMenuItems[Index].m_eType;
+			NewMenus[Index].nm_Label = a_pcoMenuItems[Index].m_pccLabel;
+			NewMenus[Index].nm_CommKey = a_pcoMenuItems[Index].m_pccHotKey;
+		}
+
+		/* Lock the default public screen and obtain a VisualInfo structure, in preparation for laying */
+		/* the menus out */
+
+		if ((Screen = IIntuition->LockPubScreen(NULL)) != NULL)
+		{
+			if ((VisualInfo = IGadTools->GetVisualInfo(Screen, TAG_DONE)) != NULL)
+			{
+				/* Create the menus and lay them out in preparation for display */
+
+				if ((m_poMenus = IGadTools->CreateMenus(NewMenus, GTMN_FrontPen, 1, TAG_DONE)) != NULL)
+				{
+					if (IGadTools->LayoutMenus(m_poMenus, VisualInfo, GTMN_NewLookMenus, 1, TAG_DONE))
+					{
+						RetVal = KErrNone;
+					}
+				}
+			}
+
+			/* And unlock the default public screen */
+
+			IIntuition->UnlockPubScreen(NULL, Screen);
+		}
+
+		/* The NewMenu structures are no longer required */
+
+		delete [] NewMenus;
+	}
+
+	return(RetVal);
+}
+
+#endif /* __amigaos__ */
+
+/* Written: Thursday 01-Jul-2010 6:46 am */
+
+TInt RApplication::Open(const struct SStdMenuItem *a_pcoMenuItems)
+{
+	TInt RetVal;
+
+	/* Assume success */
+
+	RetVal = KErrNone;
+
+	/* Create the application's menu, if requested */
+
+	if (a_pcoMenuItems)
+	{
+		RetVal = CreateMenus(a_pcoMenuItems);
+	}
+
+	return(RetVal);
 }
 
 /* Written: Monday 08-Feb-2010 6:54 am */
@@ -24,14 +131,13 @@ int RApplication::Main()
 
 #ifdef __amigaos4__
 
-	bool Done;
-	int Code;
+	int Code, Menu, MenuItem;
 	ULONG Signal, WindowSignal;
+	const struct SStdMenuItem *MenuItems;
 	struct IntuiMessage *IntuiMessage;
 
 	ASSERTM(m_poWindow, "RApplication::Main() => Window handle must not be NULL");
 
-	Done = false;
 	WindowSignal = m_poWindow->GetSignal();
 
 	do
@@ -46,7 +152,69 @@ int RApplication::Main()
 				{
 					case IDCMP_CLOSEWINDOW :
 					{
-						Done = true;
+						m_bDone = ETrue;
+
+						break;
+					}
+
+					case IDCMP_MENUPICK :
+					{
+						/* Obtain the code of the first menu item that was selected */
+
+						Code = IntuiMessage->Code;
+
+						/* Scan through the messages, processing each one.  There may be multiple */
+						/* messages waiting due to the user selecting more than one */
+
+						while (Code != MENUNULL)
+						{
+							/* Map the menu item that was selected onto its generic command ID.  The */
+							/* idea here it to iterate through the Amiga OS style menus, figuring */
+							/* out the menu and menu item number of each one, until we find one that */
+							/* matches the menu and menu item number passed in with the message */
+
+							Menu = -1;
+							MenuItem = 0;
+							MenuItems = m_pcoMenuItems;
+
+							while (MenuItems->m_eType != EStdMenuEnd)
+							{
+								/* Each time we find a menu title, the menu number increases by 1 and */
+								/* the menu item number resets.  The first title is 0, hence starting */
+								/* with -1 above */
+
+								if (MenuItems->m_eType == EStdMenuTitle)
+								{
+									++Menu;
+									MenuItem = 0;
+								}
+								else
+								{
+									/* If this menu item the one we are looking for then handle the */
+									/* command that represents the menu item */
+
+									if ((MENUNUM(Code) == Menu) && (ITEMNUM(Code) == MenuItem))
+									{
+										m_poWindow->HandleCommand(MenuItems->m_iCommand);
+
+										break;
+									}
+
+									/* Otherwise just increment the menu item # and keep searching */
+
+									else
+									{
+										++MenuItem;
+									}
+								}
+
+								++MenuItems;
+							}
+
+							/* And get the code of the next menu item selected */
+
+							Code = IIntuition->ItemAddress(m_poMenus, Code)->NextSelect;
+						}
 
 						break;
 					}
@@ -85,7 +253,22 @@ int RApplication::Main()
 			}
 		}
 	}
-	while (!(Done));
+	while (!(m_bDone));
+
+	// TODO: CAW - Move to destructor
+	/* If the menus have been created then destroy them */
+
+	if (m_poMenus)
+	{
+		/* Remove the menus from the main window, if they were added */
+
+		if (m_bMenuStripSet)
+		{
+			IIntuition->ClearMenuStrip(m_poWindow->m_poWindow);
+		}
+
+		IGadTools->FreeMenus(m_poMenus);
+	}
 
 #else /* ! __amigaos4__ */
 
@@ -131,6 +314,17 @@ void RApplication::AddWindow(CWindow *a_poWindow)
 
 	m_poWindow = a_poWindow;
 	m_poWindow->SetApplication(this);
+
+	// TODO: CAW - Is this safe to do multiple times?  Return value handling?  Matching RemoveWindow()?
+	/* Add the global application menus to the window, if they exist */
+
+	if (m_poMenus)
+	{
+		if (IIntuition->SetMenuStrip(m_poWindow->m_poWindow, m_poMenus))
+		{
+			m_bMenuStripSet = ETrue;
+		}
+	}
 }
 
 /* Written: Saturday 26-Jun-2010 2:18 pm */
@@ -139,6 +333,8 @@ void RApplication::Exit()
 {
 
 #ifdef __amigaos4__
+
+	m_bDone = ETrue;
 
 #else /* ! __amigaos4__ */
 
--- StdApplication.h
+++ StdApplication.h
@@ -6,21 +6,59 @@
 
 class CWindow;
 
+/* Enumeration defining the types of menu items that can be created. These are mapped */
+/* onto the types used by Amiga OS for easy use on that operating system */
+
+enum TStdMenuItemType
+{
+	EStdMenuEnd,
+	EStdMenuTitle,						/* A menu */
+	EStdMenuItem						/* An item on a menu */
+};
+
+/* Structure defining a single menu item to be dynamically created */
+
+struct SStdMenuItem
+{
+	TStdMenuItemType	m_eType;		/* Type of menu or menu item to be created */
+	const char			*m_pccLabel;	/* Menu item's label to be displayed */
+	const char			*m_pccHotKey;	/* Shortcut key to be displayed, if non NULL */
+	TInt				m_iCommand;		/* Command to send when menu is selected */
+};
+
 class RApplication
 {
 private:
 
-	CWindow		*m_poWindow;		/* Ptr to window used by the application */
+	CWindow			*m_poWindow;		/* Ptr to window used by the application */
 
-#ifdef WIN32
+#ifdef __amigaos__
 
-	HACCEL		m_poAccelerators;	/* Ptr to application's accelerator table, if any */
+	TBool			m_bDone;			/* Set to ETrue when it's time to exit the main loop */
+	struct Menu		*m_poMenus;			/* Ptr to main menus displayed at top of the screen */
+	TBool			m_bMenuStripSet;	/* ETrue if menus have been added to the main window */
 
-#endif /* WIN32 */
+#else /* !__amigaos__ */
+
+	HACCEL			m_poAccelerators;	/* Ptr to application's accelerator table, if any */
+
+#endif /* ! __amigaos__ */
+
+	const struct SStdMenuItem *m_pcoMenuItems;	/* Ptr to a list of structures describing the application's menus */
+
+private:
+
+#ifdef __amigaos__
+
+	TBool CreateMenus(const struct SStdMenuItem *a_pcoMenuItems);
+
+#endif /* __amigaos__ */
 
 public:
 
 	RApplication();
+
+	TInt Open(const struct SStdMenuItem *a_pcoMenuItems);
 
 	int Main();
 
