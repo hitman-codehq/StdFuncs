d39786032008d0a6d39211675398b2365a205f3f
Colin Ward <colin.ward@codehq.org>
2010-07-03 07:41:10 +0000

    Implemented RFileRequester::GetFileName() for OS4

--- StdFileRequester.cpp
+++ StdFileRequester.cpp
@@ -2,15 +2,70 @@
 #include "StdFuncs.h"
 #include "StdFileRequester.h"
 
+#ifdef __amigaos__
+
+#include <proto/asl.h>
+#include <string.h>
+
+#endif /* __amigaos__ */
+
 /* Written: Saturday 26-Jun-2010 2:48 pm */
+/* @param a_bOpen ETrue to prompt for a file to open, else EFalse for a file to save */
+/* @returns KErrNone if a filename was requested successfully */
+/*          KErrCancel if the user clicked cancel without selecting a file */
+/*          KErrNoMemory if there wasn't enough memory to allocate the requester */
+/*          KErrGeneral if another error occurred obtaining the file */
+/* Displays a native file requester prompting the user for the name of a file to open or save. */
+/* Once obtained, a ptr to this filename can be obtained by calling RFileRequester::FileName(). */
+/* Note that the filename may be 0 bytes long if the user clicked ok without selecting a file */
 
 TInt RFileRequester::GetFileName(TBool /*a_bOpen*/)
 {
 
 #ifdef __amigaos4__
 
-	// TODO: CAW
-	return(KErrGeneral);
+	TInt RetVal;
+	APTR Requester;
+	struct FileRequester *FileRequester;
+
+	/* Allocate an ASL file requester */
+
+	if ((Requester = IAsl->AllocAslRequestTags(ASL_FileRequest, TAG_DONE)) != NULL)
+	{
+		/* And display it on the screen */
+
+		if (IAsl->AslRequestTags(Requester, TAG_DONE))
+		{
+			/* Indicate success */
+
+			RetVal = KErrNone;
+
+			/* Save the name of the file for l8r use */
+
+			strcpy(m_acFileName, ((struct FileRequester *) Requester)->fr_File);
+		}
+
+		/* Either the user hit "Cancel" or the requester could not be displayed, so check to see */
+		/* what happened */
+
+		else
+		{
+			/* If DOS's last error is 0 then the user hit cancel, so indicate this.  Otherwise */
+			/* return that another error occurred */
+
+			RetVal = (IDOS->IoErr() == 0) ? KErrCancel : KErrGeneral;
+		}
+
+		/* And free the requester now that we have finished with it */
+
+		IAsl->FreeAslRequest(Requester);
+	}
+	else
+	{
+		RetVal = KErrNoMemory;
+	}
+
+	return(RetVal);
 
 #else /* ! __amigaos4__ */
 
