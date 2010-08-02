7affd6115bc69496c39edd098f3aae92b593d2c2
Colin Ward <colin.ward@codehq.org>
2010-08-02 06:28:33 +0000

    Added StdTextFile.(cpp|h) to StdFuncs suite

---
+++ StdTextFile.cpp
@@ -0,0 +1,78 @@
+
+#include <StdFuncs.h>
+
+/* Written: Friday 23-Jul-2010 8:00 am */
+
+int RTextFile::Open(const char *a_pccFileName)
+{
+	int RetVal;
+
+	/* Load the entire file into memory */
+
+	if ((RetVal = Utils::LoadFile(a_pccFileName, (unsigned char **) &m_pcBuffer)) == KErrNone)
+	{
+		m_pcBufferPtr = m_pcBuffer;
+	}
+	else
+	{
+		Utils::Info("RTextFile::Open() => Unable to read in file \"%s\"\n", a_pccFileName);
+	}
+
+	return(RetVal);
+}
+
+/* Written: Friday 23-Jul-2010 8:02 am */
+
+void RTextFile::Close()
+{
+	delete [] m_pcBuffer;
+	m_pcBuffer = m_pcBufferPtr = NULL;
+}
+
+/* Written: Friday 23-Jul-2010 8:05 am */
+/* Finds the next available line in the file, NULL terminates it and returns a ptr to it */
+/* @return	A ptr to the start of the next available line else NULL if there are no more lines */
+
+char *RTextFile::GetLine()
+{
+	char *RetVal;
+
+	/* Get a ptr to the start of the current line, which is what will be returned */
+
+	RetVal = m_pcBufferPtr;
+
+	/* Now iterate through the current line until EOF, CR or LF is found */
+
+	while ((*m_pcBufferPtr != '\0') && (*m_pcBufferPtr != 0x0d) && (*m_pcBufferPtr != 0x0a))
+	{
+		++m_pcBufferPtr;
+	}
+
+	/* If any valid characters were found then NULL terminate the current line and move the */
+	/* current line ptr to the next line */
+
+	if (m_pcBufferPtr > RetVal)
+	{
+		if (*m_pcBufferPtr != '\0')
+		{
+			/* NULL terminate the line and point the current line ptr to the next line */
+
+			*m_pcBufferPtr = '\0';
+			++m_pcBufferPtr;
+
+			if (*m_pcBufferPtr == 0x0a)
+			{
+				++m_pcBufferPtr;
+			}
+		}
+	}
+
+	/* Otherwise indicate that we have reached the EOF */
+
+	else
+	{
+		RetVal = NULL;
+	}
+
+	return(RetVal);
+}
---
+++ StdTextFile.h
@@ -0,0 +1,27 @@
+
+#ifndef STDTEXTFILE_H
+#define STDTEXTFILE_H
+
+/* This class will allow clients to read in a text file in its entirity and easily extract */
+/* lines from that file, one at a time */
+
+class RTextFile
+{
+	char	*m_pcBuffer;	/* Ptr to buffer containing the text */
+	char	*m_pcBufferPtr;	/* Ptr to current position in buffer */
+
+public:
+
+	RTextFile()
+	{
+		m_pcBuffer = NULL;
+	}
+
+	int Open(const char *a_pccFileName);
+
+	void Close();
+
+	char *GetLine();
+};
+
+#endif /* ! STDTEXTFILE_H */
