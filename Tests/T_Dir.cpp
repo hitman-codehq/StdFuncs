24363b8fa1a3d8cfa85c05c5a8c927b03ad1192c
Colin Ward <colin.ward@codehq.org>
2012-11-24 07:07:53 +0000

    Updated T_Dir.cpp test to match new directory structure

--- Tests/T_Dir.cpp
+++ Tests/T_Dir.cpp
@@ -87,7 +87,7 @@ int main()
 
 	Test.Next("Test that Open() with wildcards works");
 	TestScan("*");
-	TestScan("*.dsw", 1, 2204);
+	TestScan("*.dsw", 1, 2478);
 	TestScan("SomeDir/*.txt", 1, 174);
 
 	/* Test #5: Test that Open() with a filename works */
