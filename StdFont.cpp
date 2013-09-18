be8195c9f0b62b3f290e2dccbb64c4907bf41b4b
Colin Ward <colin.ward@codehq.org>
2013-09-18 05:24:52 +0000

    The Qt version now sets its code page to ISO 8859-15 on Linux and Windows-1252 on Windows, thus enabling the display of the Euro symbol

--- StdFont.cpp
+++ StdFont.cpp
@@ -9,6 +9,7 @@
 
 #elif defined(QT_GUI_LIB)
 
+#include <QtCore/QTextCodec>
 #include "Qt/StdWindow.h"
 
 #endif /* QT_GUI_LIB */
@@ -142,9 +143,23 @@ TInt RFont::Open(const char *a_pccFont)
 		m_iHeight = Metrics.height();
 		m_iWidth = Metrics.averageCharWidth();
 
-		/* And assign the font to the window! */
+		/* Assign the font to the window */
 
 		m_poWindow->m_poWindow->setFont(*m_poFont);
+
+		/* And set the dreaded code page.  It is crazy but Windows and Linux use two different */
+		/* code pages, thus making it impossible to display the € symbol on both systems! */
+
+#ifdef WIN32
+
+		QTextCodec::setCodecForCStrings(QTextCodec::codecForName("Windows-1252"));
+
+#else /* ! WIN32 */
+
+		QTextCodec::setCodecForCStrings(QTextCodec::codecForName("ISO 8859-15"));
+
+#endif /* ! WIN32 */
+
 	}
 	else
 	{
