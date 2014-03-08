2122b6766d0b8d0689c77f2108637bb40a4821e3
Colin Ward <colin.ward@codehq.org>
2014-03-08 05:57:08 +0000

    Updated T_Dir to work with resized Tests.dsw file

--- Tests/T_Dir.cpp
+++ Tests/T_Dir.cpp
@@ -87,7 +87,7 @@ int main()
 
 	Test.Next("Test that Open() with wildcards works");
 	TestScan("*");
-	TestScan("*.dsw", 1, 2766);
+	TestScan("*.dsw", 1, 3054);
 	TestScan("SomeDir/*.txt", 1, 174);
 
 	/* Test #5: Test that Open() with a filename works */
