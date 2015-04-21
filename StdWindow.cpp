acbc64a7c4c07035226431d6374156a027623ec6
Colin Ward <colin.ward@codehq.org>
2015-04-21 04:37:10 +0000

    #ifdef'd some code that only compiles under newer versions of MSVC

--- StdWindow.cpp
+++ StdWindow.cpp
@@ -603,7 +603,6 @@ LRESULT CALLBACK CWindow::WindowProc(HWND a_poWindow, UINT a_uiMessage, WPARAM a
 	TBool Handled;
 	HKL KeyboardLayout;
 	LRESULT RetVal;
-	UINT VirtualKey;
 	const struct SStdMenuItem *MenuItem;
 	CStdGadget *Gadget;
 	CStdGadgetLayout *LayoutGadget;
@@ -921,10 +920,14 @@ LRESULT CALLBACK CWindow::WindowProc(HWND a_poWindow, UINT a_uiMessage, WPARAM a
 				}
 			}
 
+#ifdef _WIN32_WINNT_WIN2K
+
 			/* Now take the horribleness to the next level.  Certain keys (including the mysterious */
 			/* VK_OEM_MINUS key) are simply not passed to WM_CHAR if the control key is pressed.  But */
 			/* we need these keys and they work without special treatment on other operating systems. */
 			/* So we have to look out for them individually and simulate a WM_CHAR event if found */
+
+			UINT VirtualKey;
 
 			if ((m_bCtrlPressed) && (a_uiMessage == WM_KEYDOWN))
 			{
@@ -935,6 +938,8 @@ LRESULT CALLBACK CWindow::WindowProc(HWND a_poWindow, UINT a_uiMessage, WPARAM a
 					Window->OfferKeyEvent(VirtualKey, ETrue);
 				}
 			}
+
+#endif /* _WIN32_WINNT_WIN2K */
 
 			/* Pass the raw key onto the CWindow::OfferRawKeyEvent() function, without any kind */
 			/* of preprocessing */
