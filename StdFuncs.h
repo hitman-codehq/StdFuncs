b7f91254f3f629979f33dd8a2d42dd3d87a77fe6
Colin Ward <colin.ward@codehq.org>
2016-02-15 07:00:21 +0000

    Increased the minimum required Windows version to Windows Vista in order to call CreateSymbolicLink() and GetFinalPathNameByHandle()

--- StdFuncs.h
+++ StdFuncs.h
@@ -30,7 +30,7 @@ typedef unsigned long ULONG;
 
 /* Enable newer functionality such as mouse wheel handling */
 
-#define _WIN32_WINNT 0x0501
+#define _WIN32_WINNT 0x0600
 
 #include <windows.h>
 #include <signal.h>
