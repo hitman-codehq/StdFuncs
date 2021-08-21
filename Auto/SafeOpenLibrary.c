24311b9d22bf794e4497c7bf987dcb3464d4ef6c
Colin Ward <colin.ward@codehq.org>
2021-08-21 12:15:39 +0000

    Added SafeOpenLibrary() function

---
+++ Auto/SafeOpenLibrary.c
@@ -0,0 +1,29 @@
+
+#include <proto/exec.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MINIMUM_VERSION 40
+
+/**
+ * Opens the requested library and bails out if it fails.
+ * This support function will open the requested library and, if it is not able to be opened, will print an
+ * error message and shut down the calling application.
+ *
+ * @date	Friday 20-Aug-2021 6:25 pm, By the Zwinger pond, Dresden
+ * @param	a_libraryName	The name of the library to be opened
+ * @return	A pointer the opened library, if successful
+ */
+
+struct Library *SafeOpenLibrary(const char *a_libraryName)
+{
+	struct Library *retVal;
+
+	if ((retVal = OpenLibrary(a_libraryName, MINIMUM_VERSION)) == NULL)
+	{
+		printf("Error: Unable to load library \"%s\" version %d\n", a_libraryName, MINIMUM_VERSION);
+		exit(10);
+	}
+
+	return retVal;
+}
---
+++ Auto/SafeOpenLibrary.h
@@ -0,0 +1,9 @@
+
+#ifndef SAFEOPENLIBRARY_H
+#define SAFEOPENLIBRARY_H
+
+struct Library;
+
+struct Library *SafeOpenLibrary(const char *a_libraryName);
+
+#endif /* ! SAFEOPENLIBRARY_H */
