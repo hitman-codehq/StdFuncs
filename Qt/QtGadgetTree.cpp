6b5566e180c3bd4a121df807ec76abb840438401
Colin Ward <colin.ward@codehq.org>
2026-02-14 08:54:22 +0900

    Enable multi-column display in tree gadgets
    This functionality is required for the problems window in Brunel. The
    implementation also alters the way that items are passed in so that
    it is no longer necessary to hold generic and OS native lists of
    nodes.

--- Qt/QtGadgetTree.cpp
+++ Qt/QtGadgetTree.cpp
@@ -2,22 +2,25 @@
 #include "../StdFuncs.h"
 #include "../StdWindow.h"
 #include "QtGadgetTree.h"
+#include <QtWidgets/QHeaderView>
 
 /**
  * Second phase constructor.
  * This is called by the generic tree class in order to complete Qt specific initialisation of the tree widget.
  *
  * @date	Friday 13-Aug-2021 5:33 pm, Code HQ @ Thomas's House
- * @param	a_title			Title to be displayed at the top of the tree's column
+ * @param	a_numColumns	The number of columns to display in the tree
  */
 
-void CQtGadgetTree::construct(const std::string &a_title)
+void CQtGadgetTree::construct(int a_numColumns)
 {
-	/* Set some sensible default settings, such as the number of columns to display */
-	setColumnCount(1);
-	setHeaderLabel(a_title.c_str());
+	/* Set the number of columns required and some sensible default settings */
+	setColumnCount(a_numColumns);
 	setFocusPolicy(Qt::NoFocus);
 	setSizePolicy(QSizePolicy::Minimum, QSizePolicy::MinimumExpanding);
+
+	/* Hide the header by default, or Qt will add numbers to each column if no titles are set */
+	header()->hide();
 
 	/* And connect the itemClicked() slot so that we are notified when an item is clicked */
 	connect(this, SIGNAL(itemClicked(QTreeWidgetItem*, int)), SLOT(itemClicked()));
@@ -34,4 +37,18 @@ void CQtGadgetTree::construct(const std::string &a_title)
 void CQtGadgetTree::itemClicked()
 {
 	m_parentTree->GetParentWindow()->HandleCommand(m_parentTree->GetGadgetID());
+}
+
+/**
+ * Set the title for the gadget tree.
+ * Shows the header if it is hidden and sets the title of the gadget tree.
+ *
+ * @date	Saturday 07-Mar-2026 9:16 am, Code HQ Tokyo Tsukuda
+ * @param	a_title			The string to set as the title of the tree
+ */
+
+void CQtGadgetTree::setTitle(const std::string &a_title)
+{
+	header()->show();
+	setHeaderLabel(a_title.c_str());
 }
