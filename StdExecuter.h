0adcbb66c961b6a90d180c9102e1a6590dcf1415
Colin Ward <colin.ward@codehq.org>
2026-03-28 07:52:34 +0900

    Make Win32 version of RStdExecuter asynchronous
    We now use Overlapped I/O for reading the output of applications
    launched by the RStdExecter class on Windows.

--- StdExecuter.h
+++ StdExecuter.h
@@ -56,6 +56,7 @@ private:
 
 #elif defined(WIN32)
 
+	char			*m_buffer;			/**< Buffer used to reading stdout data from the child */
 	HANDLE			m_stdInRead;		/**< Handle for reading from stdin (child) */
 	HANDLE			m_stdInWrite;		/**< Handle for writing to stdin (parent) */
 	HANDLE			m_stdOutRead;		/**< Handle for reading from stdout (parent) */
@@ -114,7 +115,11 @@ public:
 
 	void readComplete(const char *a_buffer, int a_size, const TResult &a_result);
 
-#endif /* QT_GUI_LIB */
+#else /* ! QT_GUI_LIB */
+
+	void readComplete(int a_size);
+
+#endif /* ! QT_GUI_LIB */
 
 private:
 
